bd68ffe70aed483bb72ea3e0e432c97e0da671c2
biratdatta <birat.datta26@gmail.com>
2023-10-24 21:35:07 +0530

    first commit

---
+++ diabetesprediction/diabetesprediction/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+def predict(request):
+    return render(request, 'predict.html')
+
+def result(request):
+    return render(request, 'predict.html')
